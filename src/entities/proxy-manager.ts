b61f9516f52420cb76cc1160b64a7f003828cf40
Ivang71 <artemtolv@gmail..com>
2023-11-14 16:28:39 +0300

    finish ec2 proxy test version

--- src/entities/proxy-manager.ts
+++ src/entities/proxy-manager.ts
@@ -54,6 +54,36 @@ class ProxyManager {
 }
 
 
+// const startProxyServer = async () => {
+//     while (true) {
+//         const key = keys.find((key) => !activeKeys.has(key) && key.working)
+//         if (!key) throw "Cannot find a free key. Deal with it"
+//         const port = portManager.allocatePort()
+//         if (!port) throw "Cannot find a free port for a proxy server"
+//         const localAddress = `socks5://localhost:${port}`
+//         const proxy = spawn('npx', ['--yes', '@stableness/shadowsocks-node', '--local', localAddress, '--remote', key.key])
+//         proxy.on('error', (err) => console.error(`Error starting proxy server: ${err.message}`))    
+//         proxy.on('exit', (code) => log(`Proxy server ${port} exited with code ${code}`))
+//         // wait for proxy to start. TODO make it normal
+//         await new Promise(r => setTimeout(r, 3000))
+
+//         // test that ss server works
+//         const agent = new SocksProxyAgent(localAddress, {timeout: 6000})
+//         try {
+//             const response = await fetch('http://ip-api.com/json', { agent })
+//             const ipInfo = await response.json() as IpApi
+//             return {
+//                 address: localAddress,
+//                 info: ipInfo
+//             }
+//         } catch (e) {
+//             proxy.kill()
+//             key.working = false
+//         }
+//     }
+// }
+
+
 export const proxyManager = new ProxyManager()
 
 
--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,10 @@ import fetch from 'node-fetch'
 
 import { portManager } from './entities/port-manager.ts'
 import { randChoice as chooseRandom, randint } from './utils/index.ts'
+import AWS from 'aws-sdk'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 
 const activeKeys = new Set()
@@ -19,83 +23,70 @@ const activeKeys = new Set()
 
 
 
-// const startProxyServer = async () => {
-//     while (true) {
-//         const key = keys.find((key) => !activeKeys.has(key) && key.working)
-//         if (!key) throw "Cannot find a free key. Deal with it"
-//         const port = portManager.allocatePort()
-//         if (!port) throw "Cannot find a free port for a proxy server"
-//         const localAddress = `socks5://localhost:${port}`
-//         const proxy = spawn('npx', ['--yes', '@stableness/shadowsocks-node', '--local', localAddress, '--remote', key.key])
-//         proxy.on('error', (err) => console.error(`Error starting proxy server: ${err.message}`))    
-//         proxy.on('exit', (code) => log(`Proxy server ${port} exited with code ${code}`))
-//         // wait for proxy to start. TODO make it normal
-//         await new Promise(r => setTimeout(r, 3000))
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+})
+const ec2 = new AWS.EC2()
 
-//         // test that ss server works
-//         const agent = new SocksProxyAgent(localAddress, {timeout: 6000})
-//         try {
-//             const response = await fetch('http://ip-api.com/json', { agent })
-//             const ipInfo = await response.json() as IpApi
-//             return {
-//                 address: localAddress,
-//                 info: ipInfo
-//             }
-//         } catch (e) {
-//             proxy.kill()
-//             key.working = false
-//         }
-//     }
-// }
+const launchEC2 = async (MaxCount = 1) => {
+    const proxySetupScript = `#!/bin/bash
+        sudo su
+        apt update -y
+        apt install -y squid
+        systemctl enable squid
+        cp /etc/squid/squid.conf /etc/squid/squid.conf.bak
+        sed -i 's/http_access deny all/http_access allow all/' /etc/squid/squid.conf
+        systemctl start squid
+        sudo service squid restart`
 
+    const instanceParams = {
+        ImageId: 'ami-0fc5d935ebf8bc3bc',
+        InstanceType: 't2.micro',
+        KeyName: 'default',
+        SubnetId: 'subnet-00e7eff8ecbe1774d',
+        SecurityGroupIds: ['sg-0266b224b7b575ac2'],
+        UserData: Buffer.from(proxySetupScript).toString('base64'),
+        MinCount: 1,
+        MaxCount,
+    }
 
+    const instanceData = await ec2.runInstances(instanceParams).promise()
+    const instanceIds = instanceData.Instances.map(instance => instance.InstanceId)
 
-// let a: string, i: IpApi
-// try {
-//     const { address, info } = await startProxyServer()
-//     a = address
-//     i = info
-// } catch (e) {
-//     console.error(e)
-// }
-// log(a, '\n', i)
+    await ec2.waitFor('instanceStatusOk', { InstanceIds: instanceIds }).promise()
 
+    const describeData = await ec2.describeInstances({ InstanceIds: instanceIds }).promise()
+    const publicIps = describeData.Reservations.map(reservation => reservation.Instances[0].PublicIpAddress)
 
-// i =  {
-//     status: 'success',
-//     country: 'Singapore',
-//     countryCode: 'SG',
-//     region: '01',
-//     regionName: 'Central Singapore',
-//     city: 'Singapore',
-//     zip: '069536',
-//     lat: 1.27989,
-//     lon: 103.849,
-//     timezone: 'Asia/Singapore',
-//     isp: 'OVH SAS',
-//     org: 'OVH Singapore PTE. LTD',
-//     as: 'AS16276 OVH SAS',
-//     query: '51.79.207.10'
-// }
+    return {
+        ids: instanceIds,
+        ips: publicIps,
+    }
+}
+
+const terminateEC2Instances = (instanceIds: string[]) => ec2.terminateInstances({InstanceIds: instanceIds}).promise()
 
 
-const visitTarget = async () => {
-    const browser = await chromium.launch({headless: false/*, proxy: { server: a }*/})
+
+const visitTarget = async (proxyUrl: string) => {
+    const browser = await chromium.launch({headless: false, proxy: { server: proxyUrl }})
     const context = await newInjectedContext(browser, {
         fingerprintOptions: { devices: [chooseRandom(['mobile', 'desktop'])] },
-        // newContextOptions: {
-        //     locale: i.countryCode,
-        //     timezoneId: i.timezone,
-        //     geolocation: {
-        //         latitude: i.lat,
-        //         longitude: i.lon,
-        //     },
-        // }
+        newContextOptions: {
+            locale: 'US',
+            timezoneId: 'America/New_York',
+            geolocation: {
+                latitude: 39.0438,
+                longitude: -77.4874,
+            },
+        }
     })
 
     const page = await context.newPage()
     
-    await page.goto('https://finula.online') // https://hime4qrsy1.execute-api.us-east-1.amazonaws.com/stage1/proxy
+    await page.goto('https://finula.online')
     await page.waitForSelector('body')
 
     const postLinks = await page.$$('h2.entry-title a')
@@ -106,9 +97,9 @@ const visitTarget = async () => {
     const iframe = await iframeElementHandle.contentFrame()
     await iframe.click('body')
     await page.waitForSelector('body')
-    await page.waitForTimeout(300)
+    await page.waitForTimeout(1000)
     await browser.close()
-  
+
     // for (let i = 0; i < randint(5, 10); i++) {
     //   await page.evaluate(scrollIncrement => {
     //     window.scrollBy(0, scrollIncrement)
@@ -118,134 +109,10 @@ const visitTarget = async () => {
 }
 
 try {
-    // await visitTarget()
-    const promises = Array.from({length: 10}, () => null)
-    Promise.all(promises.map(() => visitTarget()))
+    const ec2s = await launchEC2(1).catch(log)
+    // if (!ec2s) throw "No m̶a̶i̶d̶e̶n̶s̶ proxies?"
+    // await Promise.all(ec2s.ips.map(ip => visitTarget(`http://${ip}:3128`)))
+    // await terminateEC2Instances(ec2s.ids)
 } catch (e) {
     console.error(e)
 }
-
-
-
-
-
-// /* this shit doesn't crash
-// const getProxyData = async (): Promise<IpInfo | null> => {
-// 	try {
-// 		const proxy = getRandomProxy()
-// 		const timeout = 5000
-// 		const targetUrl = 'http://ip-api.com/json'
-// 		const agent = proxy.startsWith('http') ? new HttpProxyAgent(proxy, {timeout}) : new SocksProxyAgent(proxy, {timeout})
-// 		let ipDataResponse = ''
-// 		log(proxy)
-		
-// 		const response = await new Promise<string>((resolve, reject) => {
-// 			const req = http.get(targetUrl, {agent, timeout}, (res) => {
-// 				res.on('data', (chunk) => {
-// 					ipDataResponse += chunk
-// 				})
-				
-// 				res.on('end', () => {
-// 					resolve(ipDataResponse)
-// 				})
-
-// 				res.on('error', (error) => {
-// 					reject(error)
-// 				})
-
-// 				res.on('timeout', (error) => {
-// 					reject(new Error('Request timed out'))
-// 				})
-// 			})
-
-// 			req.on('error', (error) => {
-// 				reject(error)
-// 			})
-
-// 			req.end()
-// 		})
-
-// 		const ipData = JSON.parse(response)
-// 		return {
-// 			ip: ipData.query,
-// 			locale: ipData.countryCode,
-// 			timezoneId: ipData.timezone,
-// 			geolocation: {
-// 				latitude: ipData.lat,
-// 				longitude: ipData.lon,
-// 			}
-// 		}
-// 	} catch (error) {
-// 		log(error)
-// 		return null
-// 	}
-// }
-// */
-
-// const getProxyData = async (): Promise<IpInfo> => {
-// 	const proxy = getRandomProxy()
-// 	const timeout = 5000
-// 	const targetUrl = 'http://ip-api.com/json'
-// 	const agent = proxy.startsWith('http') ? new HttpProxyAgent(proxy, {timeout}) : new SocksProxyAgent(proxy, {timeout})
-// 	let ipDataResponse = ''
-
-// 	try {
-// 		return new Promise((resolve) => {
-// 			const req = http.get(targetUrl, {agent, timeout}, (res) => {
-// 				res.on('data', (chunk) => {
-// 					ipDataResponse += chunk
-// 				})
-	
-// 				res.on('end',() => {
-// 					const ipData = JSON.parse(ipDataResponse)
-// 					resolve({
-// 						ip: ipData.query,
-// 						locale: ipData.countryCode,
-// 						timezoneId: ipData.timezone,
-// 						geolocation: {
-// 							latitude: ipData.lat,
-// 							longitude: ipData.lon,
-// 						}
-// 					})
-// 				})
-
-// 				res.on('error', (error) => {
-// 					resolve(null)
-// 				})
-// 				res.on('timeout', (error) => {
-// 					resolve(null)
-// 				})
-// 			})
-			
-// 			req.on('error', (error) => {
-// 				resolve(null)
-// 			})
-// 		})
-// 	} catch (e) {
-// 		return null
-// 	}
-// }
-
-// const getWorkingProxyInfo = async () => {
-// 	let found = false, proxyInfo: Array<IpInfo | null>
-
-// 	while (!found) {
-// 		try {
-// 			proxyInfo = await Promise.all(Array(10).fill(null).map(() => getProxyData()))
-// 			log('proxyInfo', proxyInfo)
-// 			found = false
-// 		} catch (e) {
-// 			log(e)
-// 		}
-// 	}
-// 	return proxyInfo
-// }
-
-// let ipInfo
-// try {
-// 	ipInfo = await getWorkingProxyInfo()
-// } catch (e) {
-// 	log(e)
-// }
-// log('Found working proxy:', ipInfo)
-
